0acc91c6245712e41fbaa383564cdde27ed0d6c0
AlexandraG2025 <Alexandra.greenholtz2025@gbwl.org>
2024-09-17 12:58:09 +0000

    Edit main

--- Main.java
+++ Main.java
@@ -2,5 +2,26 @@ public class Main {
 
    public static void main(String []args) {
       System.out.println("Hello World");
+
+      // Problem 1
+      int speedKmS = 10; // KM PER SEC
+      int speedKmMin = speedKmS * 60;
+      int speedKmHr = speedKmMin * 60;
+      int timeDistance = speedKmHr;
+      System.out.println("It would take " + timeDistance + " hours to reach the moon");
+
+      // Promlem 2
+      int horizontal = 2000 * 2000; 
+      int vertical = 3000 * 3000;
+      int squareSpeed = horizontal * vertical;
+      System.out.println(squareSpeed);
+      // use Math.sqrt() to get the final answer 
+
+
+
+
+
+
+
    }
 }
