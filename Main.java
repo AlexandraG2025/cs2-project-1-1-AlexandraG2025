fb5ebd437d2e44dd9f17d3309ef5f391846093bd
AlexandraG2025 <Alexandra.greenholtz2025@gbwl.org>
2024-09-18 13:40:09 +0000

    edit project

--- Main.java
+++ Main.java
@@ -10,12 +10,28 @@ public class Main {
       int timeDistance = speedKmHr;
       System.out.println("It would take " + timeDistance + " hours to reach the moon");
 
-      // Promlem 2
-      int horizontal = 2000 * 2000; 
-      int vertical = 3000 * 3000;
-      int squareSpeed = horizontal * vertical;
-      System.out.println(squareSpeed);
+      // Problem 2
+      int horizontal = 2000 * 2000; // b squared
+      int vertical = 3000 * 3000; //a squared
+      int aPlusB = horizontal + vertical;
+      double totalSpeed = Math.sqrt(aPlusB);
+      System.out.println(totalSpeed); 
       // use Math.sqrt() to get the final answer 
+
+
+      // Problem 3
+      //3A
+      int radiusM = 1731 * 1000; // converted to meters
+      int radiusSquared = radiusM * radiusM; 
+      double surfaceArea = 4 * Math.PI * radiusSquared;
+      double volume = surfaceArea * 0.001; // converted to mm
+      System.out.println(volume);
+
+      //3B
+      
+
+
+
 
 
 
